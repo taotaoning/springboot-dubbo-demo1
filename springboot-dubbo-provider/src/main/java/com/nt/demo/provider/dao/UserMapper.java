eeb132a7959b86fbd625628f3d851a4eb309ba40
TaoTaoNing <790250851@qq.com>
2019-09-25 14:57:05 +0800

    数据库连接

--- springboot-dubbo-provider/src/main/java/com/nt/demo/provider/dao/UserMapper.java
+++ springboot-dubbo-provider/src/main/java/com/nt/demo/provider/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.nt.demo.provider.dao;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.nt.demo.middle.entity.User;
 import com.nt.demo.provider.utils.MyMapper;
 
@@ -7,5 +8,6 @@ import com.nt.demo.provider.utils.MyMapper;
  * Create by TaoTaoNing
  * 2019/7/25
  **/
+@DS("db1")
 public interface UserMapper extends MyMapper<User> {
 }
